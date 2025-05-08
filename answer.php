682f2c7ff0191cba11c1fabcc44dbc7f4defe42e
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-08 16:55:33 +0000

    init

--- answer.php
+++ answer.php
@@ -1,4 +1,3 @@
-index.php
 <!DOCTYPE html>
 <html>
 
@@ -30,32 +29,30 @@ index.php
       <div class="right-image">
         <img src="./images/girl-holding-division-symbol-vector-illustration-69507387.webp" alt="Division image" width="500" />
       </div>
-      <br />
-      <div class="page-content-guide">Enter two numbers to multiply:</div>
-      <div class="page-content">
-        <form action="./answer.php" method="GET">
-          <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="dividend-number" />
-            <label class="mdl-textfield__label" for="dividend-number">Your dividend number here...</label>
-            <span class="mdl-textfield__error">Input is not a number!</span>
-          </div>
-          <label>/</label>
-          <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="divisor-number" />
-            <label class="mdl-textfield__label" for="divisor-number">Your divisor number here...</label>
-            <span class="mdl-textfield__error">Input is not a number!</span>
-          </div>
-          <br />
+      <div class="page-content-answer">
+        <div id="answer">
+          <?php
+          $counter = 0;
+
+          // input
+          $dividendNumber = $_GET["dividend-number"];
+          $divisorNumber = $_GET["divisor-number"];
+
+          // process
+          while ($dividendNumber >= $divisorNumber) {
+            $dividendNumber -= $divisorNumber;
+            $counter++;
+          }
+
+          // output
+          echo $dividendNumber . ' / ' . $divisorNumber . ' is: ' . $counter . ' and ' . $dividendNumber . '/' . $divisorNumber;
+          ?>
+        </div>
       </div>
-      <br />
-      <!-- Accent-colored raised button with ripple -->
-      <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-        type="submit">
-        Calculate
-      </button>
-      </form>
-  </div>
-  </main>
+      <div class="page-content-return">
+        <a href="./index.php">Return ...</a>
+      </div>
+    </main>
   </div>
 </body>
 
